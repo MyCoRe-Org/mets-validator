bf2489009b5891200d204f86cd0289cb374d359c
Matthias Eichner <Mewel@users.noreply.github.com>
2014-10-02 15:53:59 +0000

    mets-validator: check if id's in structMap does not exist
    git-svn-id: file:///Users/thosch/test/mycore-svn/svn/maven/mets-validator/trunk@30774 7089d063-bc14-496d-ac90-53de1872eb6c

--- src/main/java/org/mycore/mets/validator/validators/StructLinkValidator.java
+++ src/main/java/org/mycore/mets/validator/validators/StructLinkValidator.java
@@ -24,6 +24,7 @@ public class StructLinkValidator implements Validator {
 
         Multimap<String, String> smLinks = getSmLinks(structLink);
 
+        // check missing
         ArrayList<String> missingLogicalDivs = new ArrayList<String>(logicalIds);
         missingLogicalDivs.removeAll(smLinks.keys());
 
@@ -37,6 +38,21 @@ public class StructLinkValidator implements Validator {
         if (!missingPhysicallDivs.isEmpty()) {
             ValidatorUtil.throwException(structLink,
                 "Some physical elements are not linked: " + missingPhysicallDivs.toString());
+        }
+
+        // check not existing
+        ArrayList<String> notExistingLogicalDivs = new ArrayList<String>(smLinks.keys());
+        notExistingLogicalDivs.removeAll(logicalIds);
+        if (!notExistingLogicalDivs.isEmpty()) {
+            ValidatorUtil.throwException(structLink, "Some linked logical elements does not exist: "
+                + notExistingLogicalDivs.toString());
+        }
+
+        ArrayList<String> notExistingPhysicalDivs = new ArrayList<String>(smLinks.values());
+        notExistingPhysicalDivs.removeAll(physicalIds);
+        if (!notExistingPhysicalDivs.isEmpty()) {
+            ValidatorUtil.throwException(structLink, "Some linked physical elements does not exist: "
+                + notExistingPhysicalDivs.toString());
         }
     }
 
@@ -66,10 +82,10 @@ public class StructLinkValidator implements Validator {
         return ids;
     }
 
-    private List<String> getPhysicalIds(Element mets) {
+    private List<String> getPhysicalIds(Element mets) throws ValidationException {
         List<String> ids = new ArrayList<String>();
         Element physicalStructMap = ValidatorUtil.getPhysicalStructMap(mets);
-        Element physSequence = physicalStructMap.getChild("div");
+        Element physSequence = ValidatorUtil.checkElement(physicalStructMap, "div");
         if (physSequence != null) {
             IteratorIterable<Element> divsIterator = physSequence.getDescendants(new ElementFilter("div",
                 ValidatorUtil.METS));
