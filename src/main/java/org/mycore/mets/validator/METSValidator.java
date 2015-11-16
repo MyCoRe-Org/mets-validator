443a87b919cbd13d6c8f70b368393e30fdcb1885
Matthias Eichner <Mewel@users.noreply.github.com>
2015-11-16 18:15:01 +0000

    add a new constructor using a jdom document
    git-svn-id: file:///Users/thosch/test/mycore-svn/svn/maven/mets-validator/trunk@33977 7089d063-bc14-496d-ac90-53de1872eb6c

--- src/main/java/org/mycore/mets/validator/METSValidator.java
+++ src/main/java/org/mycore/mets/validator/METSValidator.java
@@ -1,15 +1,30 @@
 package org.mycore.mets.validator;
 
-import org.jdom2.Document;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.located.LocatedJDOMFactory;
-import org.mycore.mets.validator.validators.*;
-
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.located.LocatedJDOMFactory;
+import org.jdom2.transform.JDOMSource;
+import org.mycore.mets.validator.validators.FileSectionValidator;
+import org.mycore.mets.validator.validators.LogicalStructMapValidator;
+import org.mycore.mets.validator.validators.PhysicalStructureValidator;
+import org.mycore.mets.validator.validators.SchemaValidator;
+import org.mycore.mets.validator.validators.StructLinkValidator;
+import org.mycore.mets.validator.validators.ValidationException;
+import org.mycore.mets.validator.validators.Validator;
 
 /**
  * Base validation class.
@@ -21,6 +36,23 @@ public class METSValidator {
     private Document document;
 
     private List<Validator> validatorList;
+
+    /**
+     * Creates a new mets validator with the document to validate.
+     * 
+     * @param doc the document to validate
+     */
+    public METSValidator(Document doc) throws JDOMException, IOException, TransformerException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        Source xmlSource = new JDOMSource(doc);
+        Result outputTarget = new StreamResult(os);
+        TransformerFactory.newInstance().newTransformer().transform(xmlSource, outputTarget);
+        InputStream is = new ByteArrayInputStream(os.toByteArray());
+        this.document = buildDocument(is);
+        this.validatorList = new ArrayList<>();
+        this.addDefaultValidators();
+        is.close();
+    }
 
     /**
      * Creates a new mets validator with the input stream to validate.
